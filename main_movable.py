9944b69c265a4f5fc9dc44739975082aa1deea6c
TarnishedAccess <tarnishedaccess@gmail.com>
2024-11-07 21:19:18 +0100

    updated movement function
    added light intensity control and upscaled the window so it doesn't look like its made for ants

--- main_movable.py
+++ main_movable.py
@@ -4,13 +4,13 @@ from camera import Camera
 from objectHandler import Sphere, Plane
 from light import Light
 
-def update_view(screen, camera, objects, screen_width, screen_height, light):
+def update_view(screen, render_surface, camera, objects, render_width, render_height, light):
     #black background, will probably replace with a floor/skybox or something later
     background_color = (135, 206, 235)
-    screen.fill(background_color)
+    render_surface.fill(background_color)
 
-    for y in range(screen_height):
-        for x in range(screen_width):
+    for y in range(render_height):
+        for x in range(render_width):
             ray = camera.castRay(x, y)
             #will probably refactor intersection detection later on so that it's based on the ray rather than the object. I think it'd be better that way?
             closest_t = np.inf
@@ -26,42 +26,47 @@ def update_view(screen, camera, objects, screen_width, screen_height, light):
                         saved_intersection_point = intersection_point
                         #find the intersection point of each object and render the closest one.
             if closest_object:
-                closest_object.render(screen, x, y, saved_intersection_point, light)
-                pygame.display.update((x, y, 1, 1))
+                closest_object.render(render_surface, x, y, saved_intersection_point, light)
             else:
-                screen.set_at((x, y), background_color)
-                pygame.display.update((x, y, 1, 1))
+                render_surface.set_at((x, y), background_color)
+
+    #we do a lil scaling
+    scaled_surface = pygame.transform.scale(render_surface, screen.get_size())
+    screen.blit(scaled_surface, (0, 0))
 
 def main():
     pygame.init()
     #if this is true, we move the camera
     #if it's false, we move the light
-    screen_width = 100
-    screen_height = 75
+    render_width = 75
+    render_height = 50
+    display_width = 900
+    display_height = 600
     camera_position = (0, 1, 0)
-    #standard FOV is 90, quake FOB is 110, play around with it if you want, looks funny.
+    #standard FOV is 90, quake FOV is 110, play around with it if you want, looks funny.
     fov = 90
-    screen = pygame.display.set_mode((screen_width, screen_height))
+    screen = pygame.display.set_mode((display_width, display_height))
     pygame.display.set_caption("Ray Tracing from Ouedkniss")
-    camera = Camera(camera_position, screen_width, screen_height, fov)
+    camera = Camera(camera_position, render_width, render_height, fov)
     moving_object = camera
     #(x, y, z), radius
-    light = Light((4, 4, 0), (200, 200, 200), 2)
+    light = Light((4, 4, 0), (255, 255, 255), 1)
     #might add multi-light support later? probably?
 
     objects = [
         #(x, y, z), radius, color
-        Sphere((0, 1, -5), 1, (200, 0, 0)),
+        Sphere((0, 1, -5), 1, (200, 50, 50)),
         Sphere((4, 2, -7), 2, (100, 100, 100)),
-        Sphere((-2, 0.5, -7), 0.5, (0, 200, 0)),
+        Sphere((-2, 0.5, -7), 0.5, (50, 200, 50)),
         #y, color
         Plane(0, ((50, 50, 50), (80, 80, 80)))
     ]
 
+    render_surface = pygame.Surface((render_width, render_height))
 
     running = True
     while running:
-        update_view(screen, camera, objects, screen_width, screen_height, light)
+        update_view(screen, render_surface, camera, objects, render_width, render_height, light)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -72,9 +77,13 @@ def main():
                 if event.key == pygame.K_RIGHT:
                     moving_object.position = (moving_object.position[0] + 1, moving_object.position[1], moving_object.position[2])
                 if event.key == pygame.K_LSHIFT:
-                    moving_object.position = (moving_object.position[0], moving_object.position[1] + 1, moving_object.position[2])
-                if event.key == pygame.K_SPACE:
                     moving_object.position = (moving_object.position[0], moving_object.position[1] - 1, moving_object.position[2])
+                if event.key == pygame.K_SPACE:
+                    moving_object.position = (moving_object.position[0], moving_object.position[1] + 1, moving_object.position[2])
+                if event.key == pygame.K_F1:
+                    light.strength += 0.25
+                if event.key == pygame.K_F2:
+                    light.strength -= 0.25
                 if event.key == pygame.K_UP:
                     moving_object.position = (moving_object.position[0], moving_object.position[1], moving_object.position[2] - 1)
                 if event.key == pygame.K_DOWN:
